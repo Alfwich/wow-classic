a75e0cd51173bef3b3835b8cf7bd54a65afbc47b
Matt Talbot <matthew.talbot@disneystreaming.com>
2019-06-22 19:41:49 -0500

    changes

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-var Discord = require('discord.io');
-var logger = require('winston');
-var auth = require('./auth.json');
+const Discord = require('discord.io');
+const logger = require('winston');
+const auth = require('./auth.json');
+
 // Configure logger settings
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
@@ -8,35 +9,42 @@ logger.add(new logger.transports.Console, {
 });
 
 logger.level = 'debug';
+
 // Initialize Discord Bot
-var bot = new Discord.Client({
-  token: auth.token,
-  autorun: true
+const bot = new Discord.Client({
+    token: auth.token,
+    autorun: true
 });
 
 bot.on('ready', function(evt) {
-  logger.info('Connected');
-  logger.info('Logged in as: ');
-  logger.info(bot.username + ' - (' + bot.id + ')');
+
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+
 });
 
 bot.on('message', function(user, userID, channelID, message, evt) {
-  // Our bot needs to know if it will execute a command
-  // It will listen for messages that will start with `!`
-  if (message.substring(0, 1) == '!') {
-    var args = message.substring(1).split(' ');
-    var cmd = args[0];
 
-    args = args.splice(1);
-    switch (cmd) {
-      // !ping
-      case 'ping':
-        bot.sendMessage({
-          to: channelID,
-          message: 'Pong!'
-        });
-        break;
-        // Just add any case commands if you want to..
+    // Our bot needs to know if it will execute a command
+    // It will listen for messages that will start with `!`
+    if (message.substring(0, 1) == '!') {
+
+        let args = message.substring(1).split(' ');
+        let cmd = args[0];
+
+        args = args.splice(1);
+
+        switch (cmd) {
+          // !ping
+          case 'ping':
+            bot.sendMessage({
+              to: channelID,
+              message: 'Pong!'
+            });
+            break;
+        }
+
     }
-  }
+
 });
